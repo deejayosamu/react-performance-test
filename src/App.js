3f4a27b231f83162a0d6ebc0d7d4d2fe85d82149
deejayosamu <seogwangwon1129@gmail.com>
2023-11-11 23:06:46 +0100

    create project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import A from './components/A';
+import B from './components/B';
 
 function App() {
+  const [value, setValue] = useState('')
+
+  const [post, setPost] = useState([])
+
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => setPost(posts))
+    }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: '1rem'}}>
+      <input 
+        value = {value}
+        onChange={e => setValue(e.target.value)}
+
+      />
+
+      <div style={{display: 'flex'}}>
+        <A message={value} posts={post}/>
+        <B message={value} posts={post}/>
+
+
+
+      </div>
     </div>
+
+
   );
 }
 
---
+++ src/components/B.js
@@ -0,0 +1,46 @@
+import React, { useCallback } from 'react'
+
+
+const Message = React.memo(({message}) => {
+  return <p>{message}</p>
+})
+
+
+const ListItem = React.memo(({post, testFunction}) => {
+  return (
+    <li key={post.id}>
+      <p>{post.title}</p>
+    </li>
+  )
+})
+
+
+const List = React.memo(({ posts }) => {
+  console.log('List component is rendering')
+  return(
+    <ul>
+      {posts.map(post => (<ListItem key={post.id} post={post}/>)
+      )}
+
+    </ul>
+  )
+})
+
+
+const B = ({message,posts}) => {
+  console.log('B component is rendering')
+  const testFunction = useCallback(() => {},[])
+  return (
+  <div> 
+    
+    <h1>B components</h1>
+    <Message message={message}/>
+    <List posts={posts} testFunction={testFunction}/>
+    
+  </div> 
+   
+
+  )
+}
+
+export default B
